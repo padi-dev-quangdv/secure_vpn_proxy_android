a7bc723dbb4b99d27d2d54458101ab00ed39b97b
Quang Do <dvquang2001@gmail.com>
2023-08-30 22:43:22 +0700

    update screen check email: resolve comments

--- app/src/main/java/com/midterm/securevpnproxy/presentation/auth/reset_password/CheckEmailFragment.kt
+++ app/src/main/java/com/midterm/securevpnproxy/presentation/auth/reset_password/CheckEmailFragment.kt
@@ -18,37 +18,28 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.midterm.securevpnproxy.R
 import com.midterm.securevpnproxy.base.BaseComposeFragment
-import com.midterm.securevpnproxy.base.compose.HandleEffect
 import com.midterm.securevpnproxy.base.compose.LocalColors
 import com.midterm.securevpnproxy.base.compose.MediumTextSemiBold
 import com.midterm.securevpnproxy.databinding.LayoutComposeOnlyBinding
-import kotlinx.coroutines.flow.Flow
 
 class CheckEmailFragment :
-    BaseComposeFragment<LayoutComposeOnlyBinding, CheckEmailViewModel>(layoutId2 = R.layout.layout_compose_only),
-    HandleEffect<CheckEmailViewModel.ViewEffect> {
-
-    override val provideEffectFlow: Flow<CheckEmailViewModel.ViewEffect>
-        get() = viewModel.effect
-
-    override fun onEffectTriggered(effect: CheckEmailViewModel.ViewEffect?) {
-    }
+    BaseComposeFragment<LayoutComposeOnlyBinding, CheckEmailViewModel>(layoutId2 = R.layout.layout_compose_only){
 
     override fun getMainComposeView(): ComposeView = binding.composeView
 
     @Composable
     override fun MainComposeViewContent(modifier: Modifier) {
         Column(
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(24.dp)
         ) {
+            Spacer(modifier = Modifier.weight(0.34f))
             ResetPasswordHeader(
                 imageTitle = R.drawable.icon_large_mail,
                 titleSrc = R.string.title_check_email,
                 descSrc = R.string.description_check_email,
             )
-            Spacer(modifier = Modifier.height(68.dp))
+            Spacer(modifier = Modifier.weight(0.08f))
             Text(
                 text = stringResource(id = R.string.forgot_password_navigate_to_login),
                 style = MediumTextSemiBold,
@@ -61,6 +52,7 @@ class CheckEmailFragment :
                         gotoLogin()
                     }
             )
+            Spacer(modifier = Modifier.weight(0.23f))
         }
     }
 
