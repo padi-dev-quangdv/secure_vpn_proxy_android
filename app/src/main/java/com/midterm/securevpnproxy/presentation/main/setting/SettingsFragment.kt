1b6afa04fff3ad72129aac651a5cf961f637df73
padi-quangdv <quangdv@paditech.com>
2023-09-13 11:31:02 +0700

    create screen settings

--- app/src/main/java/com/midterm/securevpnproxy/presentation/main/setting/SettingsFragment.kt
+++ app/src/main/java/com/midterm/securevpnproxy/presentation/main/setting/SettingsFragment.kt
@@ -1,32 +1,61 @@
 package com.midterm.securevpnproxy.presentation.main.setting
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.navigation.fragment.findNavController
 import com.midterm.securevpnproxy.R
-import com.midterm.securevpnproxy.base.BaseFragment
-import com.midterm.securevpnproxy.databinding.FragmentSettingsBinding
+import com.midterm.securevpnproxy.base.BaseComposeFragment
+import com.midterm.securevpnproxy.databinding.LayoutComposeOnlyBinding
+import com.midterm.securevpnproxy.presentation.main.ui.MainHeaderUi
 
-class SettingsFragment : BaseFragment<FragmentSettingsBinding,SettingsViewModel>(layoutId = R.layout.fragment_settings) {
+class SettingsFragment :
+    BaseComposeFragment<LayoutComposeOnlyBinding, SettingsViewModel>(layoutId2 = R.layout.layout_compose_only) {
+    override fun getMainComposeView(): ComposeView = binding.composeView
 
-
-    override fun initData() {
-    }
-
-    override fun initViewListener() {
-        binding.apply {
-            layoutHeader.iconLeft.setOnClickListener {
-                val action = SettingsFragmentDirections.actionSettingsFragmentToProfileFragment()
-                findNavController().navigate(action)
-            }
-            btnWhiteListApps.setOnClickListener {
-                val action = SettingsFragmentDirections.actionSettingsFragmentToWhiteListAppsFragment()
-                findNavController().navigate(action)
-            }
+    @Composable
+    override fun MainComposeViewContent(modifier: Modifier) {
+        Column(modifier = modifier.fillMaxSize()) {
+            MainHeaderUi(
+                titleText = getString(R.string.settings),
+                onBackClicked = {
+                    navigateBack()
+                }
+            )
+            ListFeatureSettings(
+                pinProtectClick = {
+                    /* navigate to Pin Protect*/
+                },
+                appsLockClick = {
+                    /* navigate to apps lock*/
+                },
+                customBlockListClick = {
+                    /* navigate to custom block list*/
+                },
+                whiteListAppClick = {
+                    navigateToWhiteListApp()
+                })
         }
     }
 
-    override fun initObserver() {
+    private fun navigateBack() {
+        findNavController().navigateUp()
+    }
+
+    private fun navigateToWhiteListApp() {
+        val action = SettingsFragmentDirections.actionSettingsFragmentToWhiteListAppsFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun initData() {
+
     }
 
     override fun initView() {
+
     }
+
+
 }
